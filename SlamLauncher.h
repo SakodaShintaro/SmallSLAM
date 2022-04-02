6410aabf576189eb51e9bd0f7fa97c72514c79a2
sakoda <sakoda@keio.jp>
2022-04-02 16:33:56 +0900

    とりあえずビルドできる状態でSlamLauncherを追加

---
+++ SlamLauncher.h
@@ -0,0 +1,43 @@
+#ifndef SLAM_LAUNCHER_H_
+#define SLAM_LAUNCHER_H_
+
+#include <unistd.h>
+
+#include <vector>
+
+#include "MapDrawer.h"
+#include "PointCloudMap.h"
+#include "SensorDataReader.h"
+
+class SlamLauncher {
+ private:
+  int startN;         // 開始スキャン番号
+  int drawSkip;       // 描画間隔
+  bool odometryOnly;  // オドメトリによる地図構築か
+  Pose2D ipose;       // オドメトリ地図構築の補助データ。初期位置の角度を0にする
+
+  Pose2D lidarOffset;  // レーザスキャナとロボットの相対位置
+
+  SensorDataReader sreader;  // ファイルからのセンサデータ読み込み
+  PointCloudMap *pcmap;      // 点群地図
+  MapDrawer mdrawer;         // gnuplotによる描画
+
+  // SlamFrontEnd sfront;          // SLAMフロントエンド
+  // FrameworkCustomizer fcustom;  // フレームワークの改造
+
+ public:
+  SlamLauncher() : startN(0), drawSkip(10), odometryOnly(false), pcmap(nullptr) {}
+
+  void setStartN(int n) { startN = n; }
+
+  void setOdometryOnly(bool p) { odometryOnly = p; }
+
+  void run();
+  void showScans();
+  void mapByOdometry(Scan2D *scan);
+  bool setFilename(char *filename);
+  void skipData(int num);
+  void customizeFramework();
+};
+
+#endif
